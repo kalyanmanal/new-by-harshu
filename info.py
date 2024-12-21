f2944c20408a86873e9971524aaccaa593d87782
kalyanmanal <kalyanmanal11@gmail.com>
2024-12-21 22:33:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,9 +45,7 @@ else:
     ADMINS = [int(admins) for admins in ADMINS.split()]
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '').split()]
-if len(INDEX_CHANNELS) == 0:
-    print('Info - INDEX_CHANNELS is empty')
+
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002324460500')
 if len(LOG_CHANNEL) == 0:
     print('Error - LOG_CHANNEL is missing, exiting now')
@@ -112,17 +110,7 @@ PAYMENT_QR = environ.get('PAYMENT_QR', 'https://radare.arzfun.com/api/tg/photo?i
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'manalharshal-1@oksbi')
 
 # for stream
-IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
-if len(BIN_CHANNEL) == 0:
-    print('Error - BIN_CHANNEL is missing, exiting now')
-    exit()
-else:
-    BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
-if len(URL) == 0:
-    print('Error - URL is missing, exiting now')
-    exit()
+
 else:
     if URL.startswith(('https://', 'http://')):
         if not URL.endswith("/"):
